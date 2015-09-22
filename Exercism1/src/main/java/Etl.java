7bd17673cc6e305d1d646c5245666b459d82f5e7
jorgebee65 <jorgebee65@hotmail.com>
2015-09-22 13:03:07 -0500

    Variable name changed.

--- Exercism1/src/main/java/Etl.java
+++ Exercism1/src/main/java/Etl.java
@@ -7,8 +7,8 @@ public class Etl {
     
    public Map<String, Integer> transform(Map<Integer, List<String>> old) {
        Map<String, Integer> answer = new HashMap<String, Integer>() ;
-       for(Iterator it = old.keySet().iterator(); it.hasNext();){
-           Integer key = (Integer)it.next();
+       for(Iterator iterator = old.keySet().iterator(); iterator.hasNext();){
+           Integer key = (Integer)iterator.next();
                 for(String letter : old.get(key)){
                     answer.put(letter.toLowerCase(), key);
                 }
